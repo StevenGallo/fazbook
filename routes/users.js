09b4b72021324f49ec43890e7e5614b3e9e860df
Steven Gallo <steven.gallo@outook.com>
2017-01-26 16:06:38 -0500

    Add functionality for creating new users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var models = require('.../server/models/index');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('users/index', { title: 'fazbook' });
+    models.User.findAll({}).then(function(users) {
+        res.render('user/index', {
+            title: 'fezbook',
+            users: users
+        });
+    });
 });
 
+router.get('/new', function(req, res, next) {
+    res.render('users/new', { title: 'new fazbook user' });
+});
+
+router.post('/', function(req, res, next) {
+    models.User.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        dob: req.body.dob
+    }).then(function() {
+        res.redirect('/users')
+    });
+});
 module.exports = router;
