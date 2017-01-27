d4db63de511f9dc7b367be43768dd5a2276f3987
Steven Gallo <steven.gallo@outook.com>
2017-01-26 21:17:07 -0500

    Add ability to update users

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var models = require('.../server/models/index');
+var models = require('../server/models/index');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     models.User.findAll({}).then(function(users) {
-        res.render('user/index', {
+        res.render('users/index', {
             title: 'fezbook',
             users: users
         });
@@ -26,4 +26,37 @@ router.post('/', function(req, res, next) {
         res.redirect('/users')
     });
 });
+
+router.delete('/:id', function(req, res, next) {
+    models.User.destroy({
+        where: { id: req.params.id }
+    }).then(function(user) {
+        res.redirect('/users');
+    });
+});
+
+router.get('/:id/edit', function(req, res, next) {
+    models.User.findById(req.params.id).then(function(user) {
+        res.render('users/edit', { user: user });
+    });
+});
+
+router.get('/:id', function(req, res, next) {
+    models.User.findById(req.params.id).then(function(user) {
+        console.log(user)
+        res.render('users/show', { user: user });
+    });
+});
+
+router.put('/:id', function(req, res, next) {
+    models.User.update({
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        dob: req.body.dob
+    }, { where: { id: req.params.id } }).then(function() {
+        res.redirect('/users/' + req.params.id);
+    });
+});
+
 module.exports = router;
